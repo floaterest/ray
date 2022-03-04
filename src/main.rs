e250aea8ab1f23518b1ea9172ff8781811cfff3d
Floaterest <floaterest@outlook.jp>
2022-03-04 19:19:46 -0400

    feat(main) get file from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use std::io::Result;
+use std::env;
+use crate::reader::read_text;
+
+mod linalg;
+mod cam;
+mod reader;
+
+fn main() -> Result<()> {
+    let args:Vec<String> = env::args().collect();
+    let (bs, x, y, z) = read_text(&PathBuf::from(&args[1]))?;
+    dbg!(&bs,&x,&y,&z);
+    Ok(())
 }
