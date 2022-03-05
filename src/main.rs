ee58e32f9c89045a235e43555b6b141cb8c53b9b
Floaterest <floaterest@outlook.jp>
2022-03-05 19:52:53 -0400

    feat(main) read map data and cam data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 use std::io::Result;
 use std::env;
+use crate::cam::Cam;
+use crate::math::{Ang3, Vec3};
 use crate::reader::read_text;
 
 mod math;
@@ -8,8 +10,12 @@ mod cam;
 mod reader;
 
 fn main() -> Result<()> {
-    let args:Vec<String> = env::args().collect();
-    let (bs, x, y, z) = read_text(&PathBuf::from(&args[1]))?;
-    dbg!(&bs,&x,&y,&z);
+    let args: Vec<String> = env::args().collect();
+    let (arr3, x, y, z) = read_text(&PathBuf::from(&args[1]))?;
+    let cam = Cam {
+        pos: Vec3 { x, y, z },
+        ang: Ang3 { theta: 0.0, phi: 0.0 },
+    };
+    dbg!(&arr3,&cam);
     Ok(())
 }
