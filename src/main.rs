a36c1d07aaf878b63285da4fbdde5d358f0e694c
Floaterest <floaterest@outlook.jp>
2022-03-09 19:05:08 -0400

    feat(main) can move using arrow keys

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::path::PathBuf;
-use std::io::{Result};
-// use std::env;
+use std::io::{Result, stdin, stdout, Write};
+use std::env;
 // use std::f64::consts::{FRAC_PI_2, FRAC_PI_4};
-use std::f64::consts::FRAC_PI_4;
+use std::f64::consts::{FRAC_PI_4};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 use crate::cam::{Cam, render};
 use crate::math::{Arr2, Vec3};
 use crate::reader::read_text;
@@ -13,11 +16,11 @@ mod reader;
 mod block;
 
 fn main() -> Result<()> {
-    // let args: Vec<String> = env::args().skip(1).collect();
+    let args: Vec<String> = env::args().skip(1).collect();
 
-    // let (arr3, x, y, z) = read_text(&PathBuf::from(&args[0]))?;
-    let (data, x, y, z) = read_text(&PathBuf::from("t.txt"))?;
-    let cam = Cam {
+    let (data, x, y, z) = read_text(&PathBuf::from(&args[0]))?;
+    // let (data, x, y, z) = read_text(&PathBuf::from("t.txt"))?;
+    let mut cam = Cam {
         pos: Vec3 { x, y, z },
         // theta: FRAC_PI_4,
         // phi: FRAC_PI_2,
@@ -25,14 +28,30 @@ fn main() -> Result<()> {
         phi: 0.0,
         fov2: FRAC_PI_4,
     };
-    let mut scr: Arr2<u8> = Arr2::new(36, 36, b' ');
-    render(&mut scr, &cam, &data);
+    let mut scr: Arr2<u8> = Arr2::new(100, 40, b' ');
 
-    for y in 0..scr.y {
-        for x in 0..scr.x {
-            print!("{}", scr[y][x] as char);
+    let stdout = stdout();
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+    let stdin = stdin();
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Up => cam.phi -= 0.1,
+            Key::Down => cam.phi += 0.1,
+            Key::Left => cam.theta += 0.1,
+            Key::Right => cam.theta -= 0.1,
+            Key::Char('q') => break,
+            _ => {}
         }
-        print!("\n");
+
+        render(&mut scr, &cam, &data);
+        write!(stdout, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
+        for y in 0..scr.y {
+            write!(stdout, "{}", termion::cursor::Goto(1, y as u16 + 1)).unwrap();
+            stdout.write(&scr[y]).unwrap();
+        }
+        stdout.flush().unwrap();
     }
+    stdout.write("\n".as_bytes()).unwrap();
+    stdout.flush().unwrap();
     Ok(())
 }
