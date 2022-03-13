12eb3f1c670f6b45123f0c39524a8f4097bddfe4
floaterest <56704092+floaterest@users.noreply.github.com>
2022-03-13 14:05:37 -0400

    feat 4d works

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
-use std::path::PathBuf;
-use std::io::{Result, stdin, stdout, Write};
+use std::path::{Path, PathBuf};
+use std::io::{BufReader, Read, Result, stdin, stdout, Write};
 use std::env;
+use std::fs::File;
 // use std::f64::consts::{FRAC_PI_2, FRAC_PI_4};
 // use std::f64::consts::{FRAC_PI_4};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use crate::arr4::Arr4;
 use crate::cam::{Cam, render};
-use crate::math::{Arr2, Vec3};
+use crate::math::{Arr2, Vec3, Vec4};
 use crate::reader::read_text;
 
 mod math;
 mod cam;
 mod reader;
+mod arr4;
+mod scanner;
 
 fn run() -> Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let (data, x, y, z) = read_text(&PathBuf::from(&args[0]))?;
-    // let (data, x, y, z) = read_text(&PathBuf::from("t.txt"))?;
+    let data = Arr4::from_file(String::from(&args[0]))?;
     let mut cam = Cam {
-        pos: Vec3 { x, y, z },
+        pos: Vec4 { x: 1.5, y: 1.5, z: 1.5, w: 0.0 },
         // theta: FRAC_PI_4,
         // phi: FRAC_PI_2,
         theta: 0.0,
         phi: 0.0,
         fov2: 1.0,
     };
-    let mut scr: Arr2<u8> = Arr2::new(100, 40, b' ');
+    let mut scr: Arr2<u8> = Arr2::new(200, 80, b' ');
 
     let stdout = stdout();
     let mut stdout = stdout.lock().into_raw_mode().unwrap();
@@ -48,7 +51,7 @@ fn run() -> Result<()> {
             write!(stdout, "{}", termion::cursor::Goto(1, y as u16 + 1)).unwrap();
             stdout.write(&scr[y]).unwrap();
         }
-        stdout.flush().unwrap();
+        // stdout.flush().unwrap();
     }
     stdout.write("\n".as_bytes()).unwrap();
     stdout.flush().unwrap();
@@ -56,5 +59,9 @@ fn run() -> Result<()> {
 }
 
 fn main() {
+    // let a = Arr4::from_text("test.txt").unwrap();
+    // a.save_as("test.db").unwrap();
+    // let a = Arr4::from_file("test.db").unwrap();
+    // dbg!(a.x,a.y,a.z,a.w);
     run().unwrap();
 }
