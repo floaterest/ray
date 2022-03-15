5cc20a72e56b94a897ab0e63de638cca02fb00f3
floaterest <56704092+floaterest@users.noreply.github.com>
2022-03-15 12:18:54 -0400

    feat abs rotation

--- src/cam.rs
+++ src/cam.rs
@@ -1,13 +1,28 @@
-use crate::Vec4;
+use crate::{Vec3, Vec4};
 
 #[derive(Debug)]
 pub struct Cam {
     /// current position
     pub pos: Vec4<f64>,
-    /// azimuthal angle
-    pub theta: f64,
-    /// polar angle
-    pub phi: f64,
+    // vec from eye to center of screen
+    pub pov: Vec3<f64>,
+    // vec from eye to top
+    pub top: Vec3<f64>,
     /// fov/2
     pub fov2: f64,
+}
+
+fn rotate(&src: &Vec3<f64>, &axis: &Vec3<f64>, cos: f64, sin: f64) -> Vec3<f64> {
+    src * cos + (axis * src) * sin + axis * axis.dot(&src) * (1.0 - cos)
+}
+
+impl Cam {
+    pub fn rotate_left(&mut self, cos: f64, sin: f64) {
+        self.pov = rotate(&self.pov, &self.top, cos, sin);
+    }
+    pub fn rotate_top(&mut self, cos: f64, sin: f64) {
+        let axis = self.pov * self.top;
+        self.pov = rotate(&self.pov, &axis, cos, sin);
+        self.top = rotate(&self.top, &axis, cos, sin);
+    }
 }
