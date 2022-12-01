6cac8c4590f6b0ad4d1d8d2630ab08381a264423
floaterest <56704092+floaterest@users.noreply.github.com>
2022-12-01 17:11:56 -0500

    chore(main.rs) new example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::f64::consts::*;
 use std::io::{stdout, Write};
 
@@ -18,31 +19,31 @@ mod trace;
 fn main() {
     // let args: Vec<String> = env::args().collect();
     // let mut r = Reader::new(File::open(&args[1]).unwrap());
-    let map = Map::new(Vec3::new(3, 3, 3), vec![
-        vec![vec![255; 3]; 3],
-        vec![
-            vec![255; 3],
-            vec![255, 0, 255],
-            vec![255; 3],
-        ],
-        vec![vec![255; 3]; 3],
-    ]);
+    let s = 9;
+    let map = Map::new(Vec3::new(s, s, s), (0..s).map(
+        |z| (0..s).map(
+            |y| (0..s).map(|x| (x % (s - 1), y % (s - 1), z % (s - 1))).map(
+                |(x, y, z)| if x * y + y * z + x * z == 0 { 255 } else { 0 }
+                // |(x, y, z)| if x * y * z == 0 { 255 } else { 0 }
+            ).collect()
+        ).collect()
+    ).collect());
     let cam = Camera {
         pos: Vec3::new(1.5, 1.5, 1.5),
-        forward: Vec3::new(1.0, 0.0, 0.0),
-        upward: Vec3::new(0.0, 0.0, 1.0),
+        forward: Vec3::normal(1.0, 1.0, 1.0),
+        upward: Vec3::normal(0.0, 0.0, 1.0),
         fov2: FRAC_PI_4,
     };
-    let frame = render(60, 60, &cam, &map);
+    let frame = render(472, 81, &cam, &map);
+    // let frame = render(2, 2, &cam, &map);
+    if env::args().len() > 1 { return; }
     let mut w = stdout();
-    frame.iter().for_each(
-        |row| {
-            w.write_all(&row.iter().map(|n| match n {
-                0 => b' ',
-                255 => b'#',
-                _ => b'.'
-            }).collect::<Vec<u8>>()).unwrap();
-            w.write_all(&[b'\n']).unwrap();
-        }
-    )
+    frame.iter().for_each(|row| {
+        w.write_all(&row.iter().map(|n| match n {
+            0 => b' ',
+            255 => b'.',
+            _ => b'#'
+        }).collect::<Vec<u8>>()).unwrap();
+        w.write_all(&[b'\n']).unwrap();
+    })
 }
