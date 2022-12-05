11a2abbddb606bb4655d0083eb21df3ea483c42a
floaterest <56704092+floaterest@users.noreply.github.com>
2022-12-04 19:39:47 -0500

    fix(render) ray calculation

--- src/render.rs
+++ src/render.rs
@@ -32,25 +32,33 @@ fn setup(w: usize, h: usize, cam: &Camera) -> (Vec3<f64>, Vec3<f64>, Vec3<f64>) 
     // used to shift to the next pixel
     let qx = bn * (2.0 * gx / w);
     let qy = vn * (-2.0 * gy / h);
+
     (p, qx, qy)
 }
 
 pub fn render(width: usize, height: usize, cam: Arc<Camera>, map: Arc<Map>) -> Vec<Vec<u8>> {
-    let (p, qx, qy) = setup(width, height, &cam);
+    // get top-left ray and vectors for shifting
+    let (p, dx, dy) = setup(width, height, &cam);
 
     let (n, r) = (height / NUM_THREADS, height % NUM_THREADS);
     let mid = n * (NUM_THREADS - r);
+    // split height into NUM_THREADS parts (NUM_THREADS ranges), which is divided into
+    // 1. `NUM_THREADS - r` ranges of length n
+    // 2. `r` ranges of length n+1
+    // therefore, 0..height is partitioned into NUM_THREADS sections
     let handles: Vec<JoinHandle<Vec<Vec<u8>>>> = (0..mid).step_by(n).map(|i| i..i + n).chain(
         (mid..height).step_by(n + 1).map(|i| i..i + n + 1)
     ).map(
+        // need to clone cam and map for each thread
         |range| (range, Arc::clone(&cam), Arc::clone(&map))
     ).map(|(range, cam, map)| thread::spawn(
-        move || range.map(|y| p * qy * y as f64).map(
-            |p| (0..width).map(|x| p + qx * x as f64).map(
+        move || range.map(|y| p + dy * y as f64).map(
+            |p| (0..width).map(|x| p + dx * x as f64).map(
                 |ray| Trace::new(&cam, &map, ray).last().unwrap()
             ).collect()
         ).collect()
     )).collect();
+
     let mut frame = Vec::with_capacity(height);
     for h in handles {
         frame.extend(h.join().unwrap())
